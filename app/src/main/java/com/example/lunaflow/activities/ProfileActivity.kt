43a597f51f73d1c5e1a0d39e122c8703b70f76e2
Alexandra <smir0002@hz.nl>
2026-02-08 19:25:51 +0100

    light dark mode implemented

--- app/src/main/java/com/example/lunaflow/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/lunaflow/activities/ProfileActivity.kt
@@ -1,13 +1,19 @@
 package com.example.lunaflow.activities
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.lunaflow.R
 import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : BaseActivity() {
+
+    private lateinit var prefs: SharedPreferences
 
     // inicializa activity e mostra perfil
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,9 +32,25 @@ class ProfileActivity : BaseActivity() {
         showToolbar(true)
         showBottomNav(true)
 
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
         val logoutButton = findViewById<TextView>(R.id.logoutButton)
         logoutButton.setOnClickListener {
             showLogoutConfirmation()
+        }
+
+        val darkModeSwitch = findViewById<Switch>(R.id.darkModeSwitch)
+
+        // Initialize switch state
+        val isDarkMode = prefs.getBoolean("dark_mode", false)
+        darkModeSwitch.isChecked = isDarkMode
+        setDarkMode(isDarkMode)
+
+        // Toggle listener
+        darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            setDarkMode(isChecked)
+            prefs.edit().putBoolean("dark_mode", isChecked).apply()
         }
     }
 
@@ -49,5 +71,13 @@ class ProfileActivity : BaseActivity() {
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
         finish()
+    }
+
+    private fun setDarkMode(enabled: Boolean) {
+        if (enabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
